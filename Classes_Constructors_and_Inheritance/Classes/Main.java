a2352e1a77b0004511e69d54e5b32804b9c8642b
Wuydts <42002906+Wuydts@users.noreply.github.com>
2018-09-21 12:04:48 -0500

    Create Main.java

---
+++ Classes_Constructors_and_Inheritance/Classes/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+        holden.setModel("cruz");
+        System.out.println("Model is " + porsche.getModel());
+        System.out.println("Model: " + holden.getModel());
+    }
+
+}
