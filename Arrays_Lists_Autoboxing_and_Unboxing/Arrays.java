952666f2035846b088b6c7157e597eca74fd77d0
Wuydts <42002906+Wuydts@users.noreply.github.com>
2018-09-28 12:35:03 -0500

    Update Arrays.java

--- Arrays_Lists_Autoboxing_and_Unboxing/Arrays.java
+++ Arrays_Lists_Autoboxing_and_Unboxing/Arrays.java
@@ -4,21 +4,23 @@ import java.util.Arrays;
 
 public class Main {
     /*
-        Create a program using arrays that sorts a list of integers in descending order
-        Your program should read in from the keyboard, and implement the following methods:
+        Your program should read in from the keyboard, assume valid inputs,  and implement the following methods:
         1) getIntegers - returns an array of entered integers from keyboard
         2) printArray - prints out the contents of the array
         3) sortIntegers - sorts the contents.
-
+        4) findMinAndMax - find the min. and Max Value
      */
     public static void main(String[] args) {
         Integer[] myArray = new Integer[10];
+        Integer[] minMax = new Integer[2];
         getIntegers(myArray);
         System.out.println("Before : \n");
         printArray(myArray);
         sortIntegers(myArray);
         System.out.println("\nAfter : \n");
         printArray(myArray);
+        findMinAndMax(myArray, minMax);
+        System.out.println("\nMin: " + minMax[0] + "\n" + "Max: " + minMax[1]);
     }
 
     private static void getIntegers(Integer[] myArray) {
@@ -36,7 +38,13 @@ public class Main {
     private static void sortIntegers(Integer[] myArray) {
         Arrays.sort(myArray, Collections.reverseOrder());
 
+    }
 
+    private static void findMinAndMax(Integer[] myArray, Integer[] minMax){
+        int min = Collections.min(Arrays.asList(myArray));
+        int max = Collections.max(Arrays.asList(myArray));
+        minMax[0] = min;
+        minMax[1] = max;
     }
 }
 
