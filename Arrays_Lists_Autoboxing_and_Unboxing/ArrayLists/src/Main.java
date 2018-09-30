117f2c51202b0b74029cd8ed6c4299c44e5a04ab
Wuydts <42002906+Wuydts@users.noreply.github.com>
2018-09-30 16:29:18 -0500

    Update Main.java

--- Arrays_Lists_Autoboxing_and_Unboxing/ArrayLists/src/Main.java
+++ Arrays_Lists_Autoboxing_and_Unboxing/ArrayLists/src/Main.java
@@ -36,8 +36,6 @@ public class Main {
                     searchForItem();
                     break;
                 case 6:
-                    processArrayList();
-                case 7:
                     quit = true;
                     break;
             }
@@ -82,17 +80,5 @@ public class Main {
         } else {
             System.out.println(searchItem + ", not on file.");
         }
-    }
-
-    public static void processArrayList() {
-        ArrayList<String> newArray = new ArrayList<String>();
-        newArray.addAll(groceryList.getGroceryList());
-
-        ArrayList<String> nextArray = new ArrayList<String>(groceryList.getGroceryList());
-
-        String[] myArray = new String[groceryList.getGroceryList().size()];
-        myArray = groceryList.getGroceryList().toArray(myArray);
-
-
     }
 }
